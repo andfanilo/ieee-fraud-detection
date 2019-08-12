cc1924611b1ba464e752552673d37399a23ea625
Fanilo Andrianasolo <fanilo.andrianasolo@worldline.com>
2019-08-13 00:20:11 +0200

    :sparkles: Correctly plotting binned variables with Altair

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -5,7 +5,7 @@ import plotly.express as px
 
 
 def compare_train_test_hist(X_train, X_test, col, bins):
-    fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True, figsize=(12, 4))
+    fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True, figsize=(15, 4))
 
     ax1.hist(X_train[col], bins=bins)
     ax1.set_title(f"Distribution of {col} on train")
@@ -17,7 +17,7 @@ def compare_train_test_hist(X_train, X_test, col, bins):
 
 
 def compare_isfraud_hist(X_train, col, bins):
-    fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True, figsize=(12, 4))
+    fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True, figsize=(15, 4))
 
     ax1.hist(X_train[X_train["label"] == 0][col], bins=bins)
     ax1.set_title(f"Distribution of non fraud {col}")
@@ -28,50 +28,54 @@ def compare_isfraud_hist(X_train, col, bins):
     fig.show()
 
 
-def categorical_histogram(X, col, title=None, width=400):
-    if title == None:
-        title = f"Counts of {col}"
-    feature_count = (
-        X[col]
-        .value_counts(dropna=False)
-        .reset_index()
-        .rename(columns={col: "count", "index": col})
+def compare_bins_hist_train_test(X, X_test, col, width=400):
+    return bins_histogram(X, col, f"Counts of train {col}", width) | bins_histogram(
+        X_test, col, f"Counts of test {col}", width
     )
+
+
+def compare_bins_hist_isfraud(X, col, width=800):
+    feature_count = (
+        X.groupby(col)["label"]
+        .value_counts(dropna=False)
+        .reset_index(0)
+        .rename(columns={"label": "count", col: "index"})
+        .reset_index()
+    )
+    feature_count["index"] = feature_count["index"].astype(str)
     return (
         alt.Chart(feature_count)
-        .mark_bar()
-        .encode(
-            x=alt.X(f"{col}:N", axis=alt.Axis(title=col)),
-            y=alt.Y("count:Q", axis=alt.Axis(title="Count")),
-            tooltip=[col, "count"],
-        )
-        .properties(title=title, width=width)
-    )
-
-
-def compare_cat_hist_train_test(X, X_test, col, width=400):
-    return categorical_histogram(
-        X, col, f"Counts of train {col}", width
-    ) | categorical_histogram(X_test, col, f"Counts of test {col}", width)
-
-
-def compare_cat_hist_isfraud(X, col, width=800):
-    return (
-        alt.Chart(
-            X.groupby(col)["label"]
-            .value_counts(dropna=True)
-            .reset_index(0)
-            .rename(columns={"label": "count", col: "index"})
-            .reset_index()
-        )
         .mark_bar(opacity=0.7)
         .encode(
-            x=alt.X("index:N", axis=alt.Axis(title="index")),
+            x=alt.X("index:N", axis=alt.Axis(title="index"), sort=None),
             y=alt.Y("count:Q", axis=alt.Axis(title="Count")),
             color=alt.Color("label:N"),
             tooltip=["index", "count", "label"],
         )
         .properties(title=f"Layered counts of {col}", width=width)
+    )
+
+
+def bins_histogram(X, col, title=None, width=400):
+    if title == None:
+        title = f"Counts of {col}"
+    feature_count = (
+        X[col]
+        .value_counts(dropna=False)
+        .sort_index()
+        .reset_index()
+        .rename(columns={col: "count", "index": col})
+    )
+    feature_count[col] = feature_count[col].astype(str)
+    return (
+        alt.Chart(feature_count)
+        .mark_bar()
+        .encode(
+            x=alt.X(f"{col}:N", axis=alt.Axis(title=col), sort=None),
+            y=alt.Y("count:Q", axis=alt.Axis(title="Count")),
+            tooltip=[col, "count"],
+        )
+        .properties(title=title, width=width)
     )
 
 
